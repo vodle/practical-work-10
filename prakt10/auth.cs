1765076cbb80c4f57078d532f16ed3c84cd06e44
vodle <thexhbik@gmail.com>
2019-11-19 22:50:59 +0300

    added auth screen

---
+++ prakt10/auth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt10
+{
+    class auth
+    {
+        public static int start_auth()
+        {
+            int resp;
+            Console.Write("enter\n 1.login\n 2.register\n");
+            resp = int.Parse(Console.ReadLine());
+            return resp;
+        }
+    }
+}
